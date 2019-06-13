536bc1478213a25e3c64c1758edcd61a72e7c533
Jordan Buckley <jordan@bkly.org>
2019-06-13 07:55:41 +0000

    try out material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import TextField from '@material-ui/core/TextField';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Passphrase</h1>
+        <span className="lead">Instantly generate secure, memorable passphrases</span>
       </header>
+      <main>
+        <section>
+          <TextField
+            label="Your next passphrase"
+            className="generated"
+            margin="normal"
+            variant="filled"
+          />
+        </section>
+      </main>
+      <footer>
+        <img src={logo} className="App-logo" alt="logo" />
+      </footer>
     </div>
   );
 }
