de7a8c499c7feffd7904e2a6f5d8d9661d525860
Jordan Buckley <jordan@bkly.org>
2019-06-16 06:13:12 +0000

    convert class component to functional with hooks

--- src/App.js
+++ src/App.js
@@ -1,83 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { CASING_OPTIONS, DEFAULT_OPTIONS } from './options';
 import { sampleSize, upperFirst } from 'lodash';
 
 import logo from './logo.svg';
 import './App.css';
 
-// import TextField from '@material-ui/core/TextField';
+console.log({ useState, useCallback }); // shut up linter
 
-// TODO: use HOOKS, use context
+// import TextField from '@material-ui/core/TextField';
 
 // todo: over-engineer the state management
 // todo: internationalization?
 
-export default class App extends React.Component {
-  // todo: get/save options in localstorage
-  // todo: proptypes/typing
-  constructor(props) {
-    super(props);
-    this.state = {
-      generatedPhrase: '',
-      wordList: localStorage.wordArray ? JSON.parse(localStorage.wordArray) : window.wordArray,
-      words: [],
-      options: localStorage.passphraseOptions ? JSON.parse(localStorage.passphraseOptions) : DEFAULT_OPTIONS,
-    };
-    this.generatePhrase = this.generatePhrase.bind(this);
-    this.checkPhraseLength = this.checkPhraseLength.bind(this);
-    this.copyToClipboard = this.copyToClipboard.bind(this);
-    this.handlePhraseChange = this.handlePhraseChange.bind(this);
-    this.onOptionsChange = this.onOptionsChange.bind(this);
-  }
+export default function App() {
+  const storedWordlist = localStorage.wordArray ? JSON.parse(localStorage.wordArray) : window.wordArray;
+  const initialOptions = localStorage.passphraseOptions ? JSON.parse(localStorage.passphraseOptions) : DEFAULT_OPTIONS;
+  const [wordList] = useState(storedWordlist);
+  const [options, setOptions] = useState(initialOptions);
+  const [generatedPhrase, setGeneratedPhrase] = useState('');
+  const phraseInput = useRef(null);
 
-  generatePhrase() {
-    // build phrase using lodash sampleSize - is this random enough?
-    let words = [];
-    // defaults for now - allow more customization
-    let characterLimit = this.state.options.characterLimit ? 20 : Number.MAX_SAFE_INTEGER;
-    let separator = this.state.options.useSpaces ? ' ' : '';
-
-    // generate a set of words until one is found within the character limit
-    do {
-      let wordCount = this.state.options.wordCount;
-      let number = null;
-      if (this.state.options.includeNumbers) {
-        number = getRandomInt(0, 999);
-        wordCount--;
-      }
-
-      words = sampleSize(this.state.wordList, wordCount);
-
-      if (number) {
-        let randomIndex = getRandomInt(0, wordCount + 1);
-        words.splice(randomIndex, 0, number);
-      }
-    } while (this.checkPhraseLength(words) > characterLimit);
-
-    // move logic with options?
-    switch (this.state.options.casing) {
-      case CASING_OPTIONS.upperCase:
-        words.forEach((word, i, words) => words[i] = word.toUpperCase());
-        break;
-      case CASING_OPTIONS.lowerCase:
-        words.forEach((word, i, words) => words[i] = word.toLowerCase());
-        break;
-      case CASING_OPTIONS.startCase:
-      default:
-        words.forEach((word, i, words) => words[i] = upperFirst(word));
-        break;
-      // TODO: kebab case? camel case? snake case?
-    }
-
-    let generatedPhrase = words.join(separator);
-    this.setState({words, generatedPhrase}, this.copyToClipboard);
-  }
-  checkPhraseLength(words) {
-    return words.join(this.state.options.separator).length;
-  }
-  copyToClipboard() {
+  const copyToClipboard = () => {
     // store initial cursor position
-    let el = this.phraseInput,
+    let el = phraseInput.current,
       selectionStart = el.selectionStart,
       selectionEnd = el.selectionEnd;
 
@@ -98,81 +43,132 @@ export default class App extends React.Component {
         el.setSelectionRange(selectionStart, selectionEnd);
 
       } else {
-        this.showAltMessage();
+        showAltMessage();
       }
     } catch(err) {
-      this.showAltMessage();
+      showAltMessage();
     }
 
-  }
-  handlePhraseChange(event) {
-    this.setState({ generatedPhrase: event.target.value }, this.copyToClipboard);
-  }
-  onOptionsChange(optionChanged) {
-    console.log('option changed: ', optionChanged);
+  };
 
+  const previousPhrase = useRef(generatedPhrase);
+  useEffect(() => {
+    if (previousPhrase.current !== generatedPhrase) {
+      copyToClipboard();
+    }
+  });
+
+  const checkPhraseLength = words => words.join(options.separator).length;
+
+  const generatePhrase = () => {
+    // build phrase using lodash sampleSize - is this random enough?
+    let words = [];
+    // defaults for now - allow more customization
+    let characterLimit = options.characterLimit ? 20 : Number.MAX_SAFE_INTEGER;
+    let separator = options.useSpaces ? ' ' : '';
+
+    // generate a set of words until one is found within the character limit
+    do {
+      let wordCount = options.wordCount;
+      let number = null;
+      if (options.includeNumbers) {
+        number = getRandomInt(0, 999);
+        wordCount--;
+      }
+
+      words = sampleSize(wordList, wordCount);
+
+      if (number) {
+        let randomIndex = getRandomInt(0, wordCount + 1);
+        words.splice(randomIndex, 0, number);
+      }
+    } while (checkPhraseLength(words) > characterLimit);
+
+    // move logic with options?
+    switch (options.casing) {
+      case CASING_OPTIONS.upperCase:
+        words.forEach((word, i, words) => words[i] = word.toUpperCase());
+        break;
+      case CASING_OPTIONS.lowerCase:
+        words.forEach((word, i, words) => words[i] = word.toLowerCase());
+        break;
+      case CASING_OPTIONS.startCase:
+      default:
+        words.forEach((word, i, words) => words[i] = upperFirst(word));
+        break;
+      // TODO: kebab case? camel case? snake case?
+    }
+
+    let generatedPhrase = words.join(separator);
+    setGeneratedPhrase(generatedPhrase);
+  };
+
+  const handlePhraseChange = event => {
+    setGeneratedPhrase(event.target.value);
+  };
+
+  const onOptionsChange = optionChanged => {
     if (optionChanged.hasOwnProperty('useSpaces')) {
       optionChanged.separator = optionChanged.useSpaces ? ' ' : '';
     }
 
-    const updatedOptions = Object.assign(this.state.options, optionChanged);
-    this.setState({ options: updatedOptions });
+    const updatedOptions = Object.assign({}, options, optionChanged);
+    setOptions(updatedOptions);
 
     // todo: check support, use set/get
+    // todo: useEffect
     localStorage.passphraseOptions = JSON.stringify(updatedOptions);
-  }
-  showAltMessage() {
+  };
+
+  const showAltMessage = () => {
     // todo: no jquery
     // $('#passphrase .copy-failed').show().delay(4000).fadeOut(2000);
-  }
-  render() {
-    let phrase = this.state.generatedPhrase;
+  };
 
-    return (
-      <div className="App wrapper">
-        <header id="title">
-          <h1>Passphrase</h1>
-          <p className="lead">Instantly generate secure, memorable passphrases</p>
-        </header>
+  return (
+    <div className="App wrapper">
+      <header id="title">
+        <h1>Passphrase</h1>
+        <p className="lead">Instantly generate secure, memorable passphrases</p>
+      </header>
 
-        <main id="passphrase">
-          <input
-            type="text"
-            className="generated"
-            ref={(input) => this.phraseInput = input}
-            value={this.state.generatedPhrase}
-            onChange={this.handlePhraseChange}
-            placeholder="Your next passphrase..."
-          />
-          <div className="button-group">
-            <DisplayCharCount charCount={phrase.length} />
-            <button
-              id="generate"
-              className="primary"
-              onClick={this.generatePhrase}>
-              Generate
-            </button>
-            <CopyButton onClick={this.copyToClipboard} phrase={phrase} />
-          </div>
-          <div className="message-area">
-            <p className="copied-success">Your phrase has been copied to your clipboard</p>
-            <p className="copy-failed">Press &#8984;+C (Mac) or Ctrl+C (Windows) to copy your passphrase</p>
-          </div>
-          <OptionsPanel
-            {...this.state.options}
-            onChange={this.onOptionsChange}
-          />
-        </main>
-        <footer>
-          <article className="credits">
-            <aside><a href="https://xkcd.com/936/">Why passphrases are better than passwords</a></aside>
-            <aside><a href="https://github.com/jbkly/passphrase">View the Project on GitHub</a></aside>
-            <aside><img src={logo} className="App-logo" alt="logo" /></aside>
-          </article>
-        </footer>
-      </div>
-    );
-  }
+      <main id="passphrase">
+        <input
+          type="text"
+          className="generated"
+          ref={phraseInput}
+          value={generatedPhrase}
+          onChange={handlePhraseChange}
+          placeholder="Your next passphrase..."
+        />
+        <div className="button-group">
+          <DisplayCharCount charCount={generatedPhrase.length} />
+          <button
+            id="generate"
+            className="primary"
+            onClick={generatePhrase}>
+            Generate
+          </button>
+          <CopyButton onClick={copyToClipboard} phrase={generatedPhrase} />
+        </div>
+        <div className="message-area">
+          <p className="copied-success">Your phrase has been copied to your clipboard</p>
+          <p className="copy-failed">Press &#8984;+C (Mac) or Ctrl+C (Windows) to copy your passphrase</p>
+        </div>
+        <OptionsPanel
+          {...options}
+          onChange={onOptionsChange}
+        />
+      </main>
+      <footer>
+        <article className="credits">
+          <aside><a href="https://xkcd.com/936/">Why passphrases are better than passwords</a></aside>
+          <aside><a href="https://github.com/jbkly/passphrase">View the Project on GitHub</a></aside>
+          <aside><img src={logo} className="App-logo" alt="logo" /></aside>
+        </article>
+      </footer>
+    </div>
+  );
 }
 
 function DisplayCharCount(props) {
